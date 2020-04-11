2eaeedcedfd8018b911b0826c320e61b75af25ed
Renato E. Molina <renato.molina.h@gmail.com>
2020-04-11 13:57:59 -0400

    fixed names

--- Scripts/Data.Cleaning.R
+++ Scripts/Data.Cleaning.R
@@ -2,8 +2,7 @@ library(tidyverse)
 
 
 WDB <- read_rds(here::here('data','edf_cs_ram_panel.rds')) %>%
-  rename(TAC = catch,
-         ram = ram_region,
+  rename(ram = ram_region,
          fao = primary_fao_region_name,
          cat = isscaap,
          age = age_50_mat,
